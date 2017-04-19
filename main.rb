52855e605d469bd6f37529f91bbddc0af3fcb053
Loïc Pirez <git@pirez.fr>
2017-04-19 13:54:07 +0200

    Fix null and ios

--- main.rb
+++ main.rb
@@ -11,8 +11,12 @@ class EmojiPedia
   end
 
   def self.get_webpage(url)
-    html_data = open(url).read
-    data = Nokogiri::HTML(html_data)
+    begin
+      html_data = open(url).read
+      data = Nokogiri::HTML(html_data)
+    rescue OpenURI::HTTPError => ex
+      return 'empty'
+    end
     return data
   end
 
@@ -36,7 +40,20 @@ class EmojiPedia
     return unicode_values
   end
 
-  def self.get_image_url_from_data(data, style_list)
+  def self.extract_ios(url)
+    ios = get_webpage(url)
+    if ios =~ /empty/
+      return 'empty'
+    end
+    (ios.css('.vendor-set-emoji-image').to_s.split('=')).each do |extract|
+      if extract =~ /http/
+        return extract[1..-9]
+      end
+    end
+    return 'empty'
+  end
+
+  def self.get_image_url_from_data(data, style_list, url)
     image_list = []
     image_url = 'empty'
     platform = 'empty'
@@ -54,6 +71,14 @@ class EmojiPedia
             if platform =~ /empty/
               image_url = row.split('srcset="')[1]
             else
+              if platform =~ /apple/
+                unless extract_ios('http://emojipedia.org/apple/ios-10.0/' + url.split('/')[-1]) =~ /empty/
+                  image_url = extract_ios('http://emojipedia.org/apple/ios-10.0/' + url.split('/')[-1])
+                end
+                unless extract_ios('http://emojipedia.org/apple/ios-10.3/' + url.split('/')[-1]) =~ /empty/
+                  image_url = extract_ios('http://emojipedia.org/apple/ios-10.3/' + url.split('/')[-1])
+                end
+              end
               current_image = [platform, image_url]
               image_list.push(current_image)
               platform = 'empty'
@@ -68,7 +93,7 @@ class EmojiPedia
   def self.extract_info_from_url(url, style_list)
     data = get_webpage(url)
     unicode_values = get_unicode_from_data(data)
-    image_list = get_image_url_from_data(data, style_list)
+    image_list = get_image_url_from_data(data, style_list, url)
     emoji_data = create_data_struct(unicode_values, image_list)
     return emoji_data
   end
@@ -76,7 +101,7 @@ end
 
 
 emojipedia = EmojiPedia
-
+appleold_url = '/apple/ios-10.0/'
 apple_url = '/apple/ios-10.3/'
 google_url = '/google/android-7.1/'
 windows_url = '/microsoft/windows-10-creators-update/'
@@ -89,12 +114,15 @@ twitter= '/twitter/twemoji-2.2.3/'
 firefox_os = '/mozilla/firefox-os-2.5/'
 emoji_one = '/emoji-one/2.2.5/'
 emoji_dex = '/emojidex/1.0.24/'
-style = [apple_url, google_url, windows_url, samsung,
+style = [appleold_url, apple_url, google_url, windows_url, samsung,
          lg, htc, facebook, messenger, twitter, firefox_os,
          emoji_one, emoji_dex, '/emojipedia/5.0/']
 
-emojipedia.parse_emoji_list(ARGV[0]).each do |url|
+i = 0
 
+emojipedia.parse_emoji_list(ARGV[0]).each do |url|
+  puts i
+  puts url
   emoji_data = emojipedia.extract_info_from_url(url, style)
 
   file_name = ''
@@ -103,18 +131,23 @@ emojipedia.parse_emoji_list(ARGV[0]).each do |url|
   end
   file_name = file_name[1..-1].gsub('U+', '') + '.png'
   emoji_data.image_list.each do |array|
-    unless array[1].include?('empty')
-      print 'Downloading '
-      print file_name
-      print ' as '
-      print array[0]
-      puts ' emoji style ...'
-      download_link = array[1]
-      output_file = 'output' + '/' + array[0] + '/'
-      mv_line = 'mv ' + output_file + download_link.split('/')[-1] + ' ' + output_file + file_name.downcase
-      wget_line = 'wget --quiet -N  ' + download_link + ' -P ' + output_file
-      system(wget_line)
-      system(mv_line)
+    if array[1].nil?
+      puts 'Warning : empty'
+    else
+      unless array[1].include?('empty')
+        print 'Downloading '
+        print file_name
+        print ' as '
+        print array[0]
+        puts ' emoji style ...'
+        download_link = array[1]
+        output_file = 'output' + '/' + array[0] + '/'
+        mv_line = 'mv ' + output_file + download_link.split('/')[-1] + ' ' + output_file + file_name.downcase
+        wget_line = 'wget --quiet -N  ' + download_link + ' -P ' + output_file
+        system(wget_line)
+        system(mv_line)
+      end
     end
   end
+  i += 1
 end
